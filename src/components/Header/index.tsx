db49a84591ec69707d864c2969d514ee95e130ec
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-02-03 13:54:06 -0300

    feat: updated Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,25 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import IgniteLogoPath from '../../assets/ignite-logo.svg'
+import { HeaderContainer } from './styles'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img
+        src={IgniteLogoPath}
+        alt="Dois triângulos verdes claro em 45°, sendo um menor que o outro e o maior sobrepondo o menor em seu meio, dando a sensação de movimento"
+        title="Ignite Timer"
+      />
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
