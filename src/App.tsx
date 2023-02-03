1fb3a7f5915501eb7b69d7b49f2ef437a8ab45d2
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-02-02 21:17:55 -0300

    feat: prepared theme on application

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
-  return <h1>Ignite Timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Ignite Timer</h1>
+    </ThemeProvider>
+  );
 }
